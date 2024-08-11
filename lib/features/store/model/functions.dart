bb8aea667af3ad49919bd17bf80df8ffc53c6666
Suman Shrestha <suman.shrestha@sysqube.com.np>
2024-08-11 17:34:27 +0545

    common functions

--- lib/features/store/model/functions.dart
+++ lib/features/store/model/functions.dart
@@ -1,6 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../screen/widgets/quickcart.dart';
+
 String formatNumber(num value) {
   return value.toString().replaceAllMapped(
         RegExp(r'(\d)(?=(\d{3})+(?!\d))'),
         (Match match) => '${match[1]},',
       );
+}
+
+void onCartClicked(productDetails) {
+  showModalBottomSheet(
+    enableDrag: false,
+    isScrollControlled: true,
+    context: Get.context!,
+    builder: (context) => QuickCartScreen(
+      productInfo: productDetails,
+    ),
+  );
 }
